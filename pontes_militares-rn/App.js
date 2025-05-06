52115ce7ab2ae7de64feeb142cd8fee32d7f28ce
mpdscamp <mpdscamp@gmail.com>
2025-05-05 21:41:08 -0300

    Fix code

--- pontes_militares-rn/App.js
+++ pontes_militares-rn/App.js
@@ -1,36 +1,45 @@
 import React from "react";
-
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { AppProvider } from "./context-provider/AppProvider.js";
-
 import MainScreen from "./screens/MainScreen";
 import LocaisPonteScreen from "./screens/LocaisPonteScreen";
 import RelatorioScreen from "./screens/RelatorioScreen";
+
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { StyleSheet } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <AppProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Pontes de concreto armado com vigas em T">
-          <Stack.Screen
-            name="Pontes de concreto armado com vigas em T"
-            component={MainScreen}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen
-            name="Locais das Medidas na Ponte"
-            component={LocaisPonteScreen}
-          />
-          <Stack.Screen
-            name="Adicionar dados do relatório"
-            component={RelatorioScreen}
-            options={{ headerShown: false }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </AppProvider>
+    <GestureHandlerRootView style={styles.container}>
+      <AppProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Pontes de concreto armado com vigas em T">
+            <Stack.Screen
+              name="Pontes de concreto armado com vigas em T"
+              component={MainScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen
+              name="Locais das Medidas na Ponte"
+              component={LocaisPonteScreen}
+            />
+            <Stack.Screen
+              name="Adicionar dados do relatório"
+              component={RelatorioScreen}
+              options={{ headerShown: false }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </AppProvider>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
